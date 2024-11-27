ee3b5643f2c22b2ae3c8060fa5a5ea24e6cef61e
David Griffin <Davidgriffin.2@gmail.com>
2024-11-27 00:00:31 -0500

    pushing to github

--- projects/secondary-application/src/app/app.component.ts
+++ projects/secondary-application/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddressFormComponent } from '../../../my-lib/src/lib/components/address/address-form/address-form.component';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AddressFormModel, AddressModel } from '@my-lib';
+import { DemographicsFormComponent } from '../../../my-lib/src/lib/components/demographics/demographics-form/demographics-form.component';
+import {
+  DemographicFormModel,
+  DemographicModel,
+} from 'my-lib/src/lib/components/demographics/demographics.constants';
+import { SubmitButtonGroupComponent } from '../../../my-lib/src/lib/components/buttons/submit-button-group/submit-button-group.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddressFormComponent],
+  imports: [
+    RouterOutlet,
+    AddressFormComponent,
+    DemographicsFormComponent,
+    SubmitButtonGroupComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -18,11 +30,23 @@ export class AppComponent {
     addressLineOne: '2745 Towne Blvd',
   };
 
+  demographicObj: Partial<DemographicModel> = {
+    firstName: 'David',
+  };
+
   parentForm = new FormGroup({
     addressForm: AddressFormComponent.createForm(this.fb, this.addressObj),
+    demographicForm: DemographicsFormComponent.createForm(
+      this.fb,
+      this.demographicObj
+    ),
   });
 
   get addressForm(): FormGroup<AddressFormModel> {
     return this.parentForm.controls.addressForm;
+  }
+
+  get demographicForm(): FormGroup<DemographicFormModel> {
+    return this.parentForm.controls.demographicForm;
   }
 }
